efc4b1da2e629eba56d7d7b02c692ff05439bce1
nole2701 <nole2701@gmail.com>
2023-02-28 21:07:56 -0500

    removed commented code

--- src/tuiter/who-to-follow/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow/who-to-follow-list-item.js
@@ -20,22 +20,3 @@ const WhoToFollowListItem = ({who}) => {
  );
 };
 export default WhoToFollowListItem;
-
-// // who refers to the handle of the user since all handles are unique.
-// const WhoToFollowListItem = (who) => {
-//     return(
-//     <div className="col-12 border-top border-secondary bg-light form-inline align-middle justify-content-between">
-//         <div className="form-inline align-middle">
-//             <div><img src="{who.avatarIcon}" className="rounded-circle m-1 mr-3" height="48" width="48"/></div>
-//             <div>
-//                 <div className="font-weight-bold">{who.userName}<i className="fa fa-solid fa-check-circle ml-1"></i></div>
-//                 <div>{who.handle}</div>
-//             </div>
-//         </div>
-//         <div className="border border-secondary  bg-primary form-inline rounded-pill justify-content-md-center">
-//             <span className="py-1 px-2 text-center align-middle text-white">Follow</span></div>
-//     </div>
-//     );
-//    }
-//    export default WhoToFollowListItem;
-
