21ff2f337ecf237ba0b0c46382a7229d75b03895
nole2701 <nole2701@gmail.com>
2023-04-05 17:06:28 -0400

    finished a9

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-// const TUITS_API = 'https://tuiter-node-server-app-ewru.onrender.com/api/tuits';
+// const TUITS_API = 'https://tuiter-node-server-app-ewru.onrender.com/api/tuits'; (NO MONGODB)
+// const TUITS_API = 'https://tuiter-node-server-app-a9-dmy2.onrender.com/api/tuits'; (MONGODB)
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
